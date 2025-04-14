99bcb3e44b83fc2e93abcf7fa0aa1425a766c2c4
ImEldie <daniel.frankort@telenet.be>
2025-04-14 21:49:26 +0200

    Add gradient to card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Card = ({ title, content, image, children }) => {
   return (
-    <div className="bg-background-900 bg-gradient-to-tr from-secondary-900 to-primary-800 rounded-2xl shadow-md overflow-hidden w-80 m-5">
-      {image && <img src={image} alt="Card" className="w-full h-48 object-cover" />}
+    <div className="m-5 overflow-hidden shadow-md bg-background-900 bg-gradient-to-tr from-secondary-900 to-primary-800 rounded-2xl w-80 group">
+      {image && (
+        <div className="relative w-full h-48">
+          <img src={image} alt="Card" className="object-cover w-full h-full" />
+          <div className="absolute inset-0 transition-opacity opacity-40 group-hover:opacity-20 bg-gradient-to-t from-primary-950 to-primary-400" />
+        </div>
+      )}
+
       <div className="p-5 text-left">
-        {title && <h2 className="text-xl font-semibold mb-4">{title}</h2>}
-        <p className="text-base mb-4">{content}</p>
+        {title && <h2 className="mb-4 text-xl font-semibold">{title}</h2>}
+        <p className="mb-4 text-base">{content}</p>
         {children && <div className="text-sm text-right">{children}</div>}
       </div>
     </div>
