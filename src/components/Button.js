95655b9aa8ac1a70ad902613b05a0f3ff2780edd
ImEldie <daniel.frankort@telenet.be>
2025-04-14 21:13:58 +0200

    Adjust component colors

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,24 +3,27 @@ import React from "react";
 const Button = ({ children, href }) => {
   const content = children || "Click Me!";
 
+  // Anchor button
   if (href) {
     return (
       <a
         target="_blank"
         href={href}
-        className="bg-primary-400 text-text-50 py-2 px-4 rounded-md hover:bg-primary-300 transition-all inline-block"
+        className="bg-secondary-500 hover:bg-secondary-400 hover:text-text-50 hover:rotate-1 py-2 px-4 rounded-3xl transition-all inline-block"
       >
         {content}
       </a>
     );
-  } else {
-    return (
-      // ToDo => Do I need javascript click events?
-      <button className="bg-primary-400 text-text-50 py-2 px-4 rounded-md hover:bg-primary-300 transition-all">
-        {content}
-      </button>
-    );
   }
+
+  // Default button
+  return (
+    // ToDo => Do I need javascript click events?
+    <button className="bg-secondary-500 hover:bg-secondary-400 hover:text-text-50 hover:rotate-1 py-2 px-4 rounded-3xl transition-all inline-block">
+      {content}
+      <div>PLACEHOLDER</div>
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, content, image, children }) => {
   return (
-    <div className="bg-background-900 bg-gradient-to-tr from-secondary-900 to-background-900 rounded-2xl shadow-md overflow-hidden w-80 m-5">
+    <div className="bg-background-900 bg-gradient-to-tr from-secondary-900 to-primary-800 rounded-2xl shadow-md overflow-hidden w-80 m-5">
       {image && <img src={image} alt="Card" className="w-full h-48 object-cover" />}
       <div className="p-5 text-left">
         {title && <h2 className="text-xl font-semibold mb-4">{title}</h2>}
