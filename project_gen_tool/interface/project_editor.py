7d7d4cf71721aa2970a65f05912de5d04515d88b
ImEldie <daniel.frankort@telenet.be>
2025-04-17 21:13:33 +0200

    Add generate from README.md button

--- project_gen_tool/interface/project_editor.py
+++ project_gen_tool/interface/project_editor.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from utils.content import Content
 
 class ProjectEditor:
@@ -38,12 +39,25 @@ class ProjectEditor:
         self.excerpt = tk.Text(self.frame, width=50, height=4)
         self.excerpt.pack(pady=5, anchor="w", fill=tk.X)
 
-        tk.Label(self.frame, text="Description:").pack(anchor="w")
+        description_frame = tk.Frame(self.frame)
+        description_frame.pack(anchor='w', pady=5, fill=tk.X)
+        tk.Label(description_frame, text="Description:").pack(side='left')
+        tk.Button(description_frame, text="[AI] Generate from README.md", command=self._generate_from_readme, bg="lightblue").pack(side="right", padx=10)
         self.description = tk.Text(self.frame, width=50, height=10)
         self.description.pack(pady=5, anchor="w", fill=tk.X)
 
         self.save_btn = tk.Button(self.frame, text="Save Changes")
         self.save_btn.pack(pady=(0, 5), fill=tk.X)
+
+    def _generate_from_readme(self):
+        if self.data.description != '':
+            confirm = messagebox.askyesno("Confirm", "This will overwrite the current description, are you sure?")
+            if not confirm:
+                return
+
+        self.data.generate_description()
+        self.description.delete("1.0", tk.END)
+        self.description.insert("1.0", self.data.description)
 
     def save_changes(self):
         self.data.display_name = self.name.get()
@@ -58,6 +72,9 @@ class ProjectEditor:
     def open_project(self, project: Content):
         self.project = project
         self.data = Content(project.to_dict(), self.project.path)
+
+        print(self.project.to_dict())
+        print(self.data.to_dict())
 
         self.name.delete(0, tk.END)
         self.name.insert(0, project.display_name)
