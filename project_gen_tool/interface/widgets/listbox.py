aa886586dc42b6f9acf16e3bb4bb156c56dc85c9
ImEldie <daniel.frankort@telenet.be>
2025-04-21 20:31:49 +0200

    Clean up listbox label

--- project_gen_tool/interface/widgets/listbox.py
+++ project_gen_tool/interface/widgets/listbox.py
@@ -18,9 +18,9 @@ class _Listbox:
         self.name = self.get_path().name
         self.projects = []
 
-        tk.Label(frame, text=self.name.capitalize()).pack()
+        tk.Label(frame, text=self.name.capitalize()).pack(fill=tk.X, anchor="w")
         self.listbox = tk.Listbox(frame, width=40, height=20)
-        self.listbox.pack(pady=10)
+        self.listbox.pack(pady=(0, 10))
 
         self.update_list()
         self._start_watcher()
