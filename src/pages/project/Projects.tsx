05f630e4be8d0417e986f312024301daa437c81e
ImEldie <daniel.frankort@telenet.be>
2025-04-21 21:58:09 +0200

    Add basic index.json navigation functionality

--- src/pages/project/Projects.tsx
+++ src/pages/project/Projects.tsx
@@ -1,7 +1,27 @@
+import React, { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import Button from "../../components/Button";
 
+export interface Project {
+  file: string;
+  display_name: string;
+  excerpt: string;
+}
+
 function Projects() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch("/content/index.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects(data.repos);
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error);
+      });
+  }, []);
+
   return (
     <div className="text-center">
       <div className="space-y-8">
@@ -11,9 +31,17 @@ function Projects() {
             content="I have many projects you can see on this website, feel free to take a look around!"
           >
             <div className="button-list">
-              <Button href="https://github.com/DaFrankort">Java</Button>
-              <Button href="https://github.com/DaFrankort">Python</Button>
-              <Button href="https://github.com/DaFrankort">Typescript</Button>
+              {projects.length > 0 ? (
+                // Loop through the projects and create a button for each
+                projects.map((project) => (
+                  <div key={project.file} className="project-item">
+                    <Button href={`project/${project.file.replace(".json", "")}`}>{project.display_name}</Button>
+                    <p>{project.excerpt}</p>
+                  </div>
+                ))
+              ) : (
+                <p>Loading projects...</p>
+              )}
             </div>
           </Card>
         </section>
