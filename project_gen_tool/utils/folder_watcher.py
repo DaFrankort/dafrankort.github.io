028a48557912ea97d6f645633736bd552e516074
ImEldie <daniel.frankort@telenet.be>
2025-04-21 17:13:36 +0200

    Rename FolderEventHandler to _FolderEventHandler

--- project_gen_tool/utils/folder_watcher.py
+++ project_gen_tool/utils/folder_watcher.py
@@ -4,7 +4,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import threading
 
-class FolderEventHandler(FileSystemEventHandler):
+class _FolderEventHandler(FileSystemEventHandler):
     def __init__(self, update_callback: Callable[[], None]):
         self.update_callback = update_callback
 
@@ -19,7 +19,7 @@ class FolderEventHandler(FileSystemEventHandler):
 
 class FolderWatcher:
     def __init__(self, watch_path: Path, on_update: Callable):
-        event_handler = FolderEventHandler(on_update)
+        event_handler = _FolderEventHandler(on_update)
 
         self.obs = Observer()
         self.obs.schedule(event_handler, watch_path, recursive=False)
