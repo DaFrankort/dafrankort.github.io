745bfb5d3a01f93366dd3a8bce990f1866dc3d5f
ImEldie <daniel.frankort@telenet.be>
2025-04-27 12:47:59 +0200

    Rework techstack chip-list functionality

--- src/pages/projects/partials/ProjectCard.tsx
+++ src/pages/projects/partials/ProjectCard.tsx
@@ -2,31 +2,34 @@ import Card from "../../../components/Card";
 import Button from "../../../components/Button";
 import { IndexProject } from "../../../functions/FetchIndex";
 import Chip from "../../../components/Chip";
+import { useIsMediumScreen } from "../../../functions/UseMediaQuery";
 
 type ProjectCardProps = {
   project: IndexProject;
 };
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
-  const maxChipCount = 3;
+  const maxChipCount = useIsMediumScreen() ? 5 : 3;
 
   return (
     <Card
-      title={project.display_name}
       content={
-        <div>
-          <div className="mb-1 -mt-3 chip-list-sm">
-            {project.techstack.slice(0, maxChipCount).map((techstack, index) => (
-              <Chip key={index} text={techstack} className="opacity-90" />
-            ))}
-            {project.techstack.length > maxChipCount && (
-              <Chip
-                key={"more"}
-                text={`+ ${project.techstack.length - maxChipCount} ${
-                  project.techstack.length - maxChipCount === 1 ? "other" : "others"
-                }...`}
-                className="opacity-60"
-              />
+        <div className="space-y-1">
+          <h3>{project.display_name}</h3>
+          <div className="chip-list-sm">
+            {project.techstack.length <= maxChipCount ? ( // Show max 3 chips
+              project.techstack.map((tech, index) => <Chip key={index} text={tech} className="opacity-90" />)
+            ) : (
+              <>
+                {project.techstack.slice(0, maxChipCount - 1).map((tech, index) => (
+                  <Chip key={index} text={tech} className="opacity-90" /> // More than 3 techstacks, => 3rd chip becomes "More" chip
+                ))}
+                <Chip
+                  key="more"
+                  text={`+ ${project.techstack.length - (maxChipCount - 1)} others...`} // case of 1 other won't happen because then we'd just show the techstack
+                  className="opacity-60"
+                />
+              </>
             )}
           </div>
           <p>{project.excerpt}</p>
